6fd25c66f2c1f056a96f21e443e8ef283829827a
EddieLo <eddielo106@gmail.com>
2018-08-28 16:09:45 -0700

    complete base csv generator

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,56 @@
+console.log('App.js is connected');
 
+class App {
+  constructor() {
+    console.log('App was instantiated')
+    this.server = 'http://localhost:3000/data';
+  }
 
+  init() {
+    $('#submit').click((event) => {
+      event.preventDefault();
+      this.post($('#input').val());
+    });
+  }
+
+  post(data) {
+    // console.log('data: ', data)
+
+    $.ajax({
+      url: this.server,
+      type: 'POST',
+      contentType: 'application/json',
+      data: data,
+      success: () => {
+        this.get();
+      },
+      error: (data) => {
+        console.log(data);
+      }
+    });
+  }
+
+  get(input) {
+    $.ajax({
+      url: this.server,
+      type: 'GET',
+      contentType: 'application/json',
+      success: (data) => {
+        data = data.split('\n');
+        $('#csv').empty();
+        for (let elem of data) {
+          $('#csv').append(`${elem}<br>`);
+        }
+      },
+      error: (data) => {
+        console.log(data);
+      }
+    });
+  }
+};
+
+const app = new App();
+
+$( document ).ready(() =>{
+  app.init();
+});
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,33 @@
+const express = require('express');
+const app = express();
+const controller = require('./controller')
 
+// middleware
+const parser = require('body-parser');
 
+app.set('port', 3000);
+
+// use middleware
+app.use(parser.json());
+
+// static files
+app.use('/', express.static('client'));
+
+// set-up routing
+app.post('/data', (req, res) => {
+  console.log('POST Request')
+  controller.post(req.body, () => {
+    res.send();
+  });
+});
+
+app.get('/data', (req, res) => {
+  console.log('GET Request')
+  controller.get((data) => {
+    res.send(data);
+  });
+});
+
+app.listen(app.get('port'), () => {
+  console.log('app is listening to port 3000')
+});
